1006b7f11e92eef7f0f6b46559396015976ed5b4
Rawan Eldamanhory <rawaneldamanhory@gmail.com>
2026-01-08 16:44:13 +0200

    Initial commit: Demonstrate exception handling in C# using try-catch-finally

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] num = new int[3];
+                num[3] = 23;
+            }
+            catch(System.IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("That is the final code.");
+            }
+        }
+    }
+}
